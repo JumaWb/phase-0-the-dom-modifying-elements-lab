bcbdafed4d8813291c04298a36d824d1ebf55dbe
Brian Juma <bjuma0985@gmail.com>
2024-04-01 12:50:45 +0300

    dom-modifying

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const myMain= document.getElementById("main")
+myMain.remove();
+const newHeader = document.createElement("h1")
+newHeader.id= "victory";
+newHeader.textContent = "brian is the champion"
